58ba6b30035f3b4444bda8388ebc72fe65ecbf7d
uditagarwal <qzbxq2d3kn56zkxa6uhqqfv3vvecbpmmqq25dg3xut5hiins23qa>
2022-03-16 17:02:08 +0530

    final changes

--- src/v1/util/cron_jobs.js
+++ src/v1/util/cron_jobs.js
@@ -3,8 +3,7 @@ var cron = require('node-cron');
 const { reset_daily_score } = require('../util/score');
 
 const task = cron.schedule("0 0 0 * * *", () => {
-    // reset_daily_score();
-    console.log("10 seconds");
+    reset_daily_score();
 })
 
 module.exports = task;
